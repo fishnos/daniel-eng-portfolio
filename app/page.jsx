2a421a89eb2d618dd9f676ce0966fbb6f3c010e8
fishnos <dkny11692@gmail.com>
2025-06-18 20:33:07 -0400

    Chopped animations

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,8 @@
 
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion";
+import { useRef, useEffect } from "react";
+import { motion, useInView, useAnimation, AnimatePresence } from "framer-motion";
 
 export default function HomePage() {
   const handleClickEvent = (e) => {
@@ -15,70 +16,88 @@ export default function HomePage() {
   };
 
   return (
-    <motion.section
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: {
-          delay: 1.7,
-          duration: 0.4,
-          ease: "easeIn",
-        },
-      }}
-      className="h-screen"
-    >
-      <div className="flex flex-col w-full items-center xl:flex-row">
+    <>
+      <div className="h-screen">
         {/*name card*/}
-        <div className="min-h-screen flex items-center justify-center relative w-full">
-          <div className="bg-white/10 backdrop-blur-lg border border-white/20 shadow-xl rounded-2xl p-10 w-full text-center">
-            <h1 className="text-4xl md:text-5xl font-extrabold text-secondary mb-4">
-              Hello, I'm{" "}
-              <span className="text-gradient text-glow-hover">Daniel Kosukhin</span>
-            </h1>
-            <TypeAnimation
-              sequence={[
-                "Front-End Developer",
-                2000,
-                "Robotics Engineer",
-                2000,
-                "Problem-Solver",
-                2000,
-              ]}
-              className="text-xl text-gray-200 mb-4"
-              wrapper="span"
-              speed={40}
-              repeat={Infinity}
-              cursor={false}
-            />
-            <p className="text-gray-300 mb-4 mt-2">
-              I specialize in creating technology that solves problems with
-              optimality and practicality.
-            </p>
-            <div className="flex justify-center gap-4">
-              <a
-                href="/projects"
-                className="px-6 py-2 bg-white/20 hover:bg-white/30 text-white rounded-md font-medium transition"
-              >
-                View My Work →
-              </a>
-              <a
-                onClick={handleClickEvent}
-                className="px-6 py-2 bg-white/20 hover:bg-white/30 text-white rounded-md font-medium transition"
-              >
-                Get In Touch
-              </a>
+        <AnimatePresence mode="sync">
+          <motion.div 
+            initial={{opacity: 0, y: 75}}
+            whileInView={{
+              opacity: 1, 
+              y: 0,
+              transition: {
+                delay: 1.7,
+                duration: 0.55,
+                ease: "circInOut",
+              },
+            }}
+            viewport={{ once: true }}
+            className="flex flex-col w-full items-center xl:flex-row"
+          >
+            <div className="min-h-screen flex items-start mt-20 justify-center relative w-full overflow-hidden">
+              <div className="bg-white/10 backdrop-blur-lg border border-white/20 shadow-xl rounded-2xl p-10 w-full text-center">
+                <h1 className="text-4xl md:text-5xl font-extrabold text-secondary mb-4">
+                  Hello, I'm{" "}
+                  <span className="text-gradient text-glow-hover">Daniel Kosukhin</span>
+                </h1>
+                <div className="h-[30px] overflow-hidden">
+                  <TypeAnimation
+                    sequence={[
+                      "Front-End Developer",
+                      2000,
+                      "Robotics Engineer",
+                      2000,
+                      "Problem-Solver",
+                      2000,
+                    ]}
+                    className="text-xl text-gray-200 mb-4 whitespace-nowrap"
+                    wrapper="span"
+                    speed={40}
+                    repeat={Infinity}
+                    cursor={false}
+                  />
+                </div>
+                <p className="text-gray-300 mb-4 mt-2">
+                  I specialize in creating technology that solves problems with
+                  optimality and practicality.
+                </p>
+                <div className="flex justify-center gap-4">
+                  <a
+                    href="/projects"
+                    className="px-6 py-2 bg-white/20 hover:bg-white/30 text-white rounded-md font-medium transition"
+                  >
+                    View My Work →
+                  </a>
+                  <a
+                    onClick={handleClickEvent}
+                    className="px-6 py-2 bg-white/20 hover:bg-white/30 text-white rounded-md font-medium transition"
+                  >
+                    Get In Touch
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="flex-1 hidden xl:flex relative z-20"></div>
+            <div className="flex-1 hidden xl:flex relative z-20"></div>
+          </motion.div>
+        </AnimatePresence>
         {/*projects display*/}
-        <motion.section
-          initial={{}}
-          className=""
+        <motion.h1
+          initial={{opacity: 0, y: 75}}
+          whileInView={{
+            opacity: 1, 
+            y: 0,
+            transition: {
+              delay: 0.3,
+              duration: 0.55,
+              ease: "circInOut",
+            },
+          }}
+          viewport={{ once: true }}
+          className="flex justify-start h-full w-full z-30"
         >
-          
-        </motion.section>
+          Projects
+        </motion.h1>
       </div>
-    </motion.section>
+    </>
   );
 }
